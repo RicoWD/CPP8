6bcf31d849c18f5c7a5f5621af09d1da55ebe676
erpascua <erpascua@student.42.fr>
2026-03-04 19:43:32 +0100

    (ex00): Makefile done, init of exercice

---
+++ ex00/includes/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 19:36:17 by erpascua          #+#    #+#             */
+/*   Updated: 2026/03/04 19:42:37 by erpascua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <string>
+# include <iostream>
+
+template <typename T>
+void easyfind(T intContainer, int)
+{
+	for (int i = 0; i < intContainer)
+	
+}
