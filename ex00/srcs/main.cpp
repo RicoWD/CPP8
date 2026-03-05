707819aa88f19191064893a0c121990df9cff3cb
RicoWD <pascual.machio.eric@gmail.com>
2026-03-05 02:29:27 +0100

    (ex00): Main tested more precise

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 01:17:24 by erpascua          #+#    #+#             */
-/*   Updated: 2026/03/05 02:03:46 by erpascua         ###   ########.fr       */
+/*   Updated: 2026/03/05 02:27:17 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,42 @@
 
 int main()
 {
-	int				arr[] = {123, 21, 42, 43, 4};
-	std::vector<int>	nbs(arr, arr + 5);
+	std::vector<int>	nbs;
+	nbs.push_back(123);
+	nbs.push_back(21);
+	nbs.push_back(42);
+	nbs.push_back(43);
+	nbs.push_back(4);
 
-	try { easyfind(nbs, 42); }
-	catch (std::exception &e) { std::cerr << e.what() << std::endl; }
+	try 
+	{
+		easyfind(nbs, 42); 
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl; 
+	}
 
-	try { easyfind(nbs, 99); }
-	catch (std::exception &e) { std::cerr << e.what() << std::endl; }
+	try 
+	{
+		easyfind(nbs, 99);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	nbs.push_back(99);
+	// nbs.pop_back();
+	
+	try 
+	{
+		easyfind(nbs, 99);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 
 	return (0);
 }
