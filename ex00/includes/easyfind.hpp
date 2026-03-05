3db28d1b13b70fe2a09ac70057805074b0defa0b
erpascua <erpascua@student.42.fr>
2026-03-05 20:42:33 +0100

    (ex00): Exercice finished

--- ex00/includes/easyfind.hpp
+++ ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 19:36:17 by erpascua          #+#    #+#             */
-/*   Updated: 2026/03/05 02:07:36 by erpascua         ###   ########.fr       */
+/*   Updated: 2026/03/05 13:26:38 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ typename T::const_iterator easyfind(T const &container, int n)
 {
 	typename T::const_iterator it = std::find(container.begin(), container.end(), n);
 	
+	// std::cout << "End value |" << *(container.end() -1) << "|" << std::endl;
 	if (it == container.end())
 		throw std::runtime_error("Value not found in container");
 
